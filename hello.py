57a110b2307b8755056c99e6f730947cf189970c
guyzamir12 <zamir123321@gmail.com>
2026-01-31 21:19:27 +0200

    delete files
    Signed-off-by: guyzamir12 <zamir123321@gmail.com>

--- hello.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-# Get current time
-now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-print(f"[{now}] 🚀 Jenkins is running this Python script!")
-print("Everything looks good from here.")
